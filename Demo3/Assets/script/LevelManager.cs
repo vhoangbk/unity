75351797550912bdbefccde76d065dc10aba89ee
HoangNV <hoangnv.paraline@gmail.com>
2016-03-02 17:34:32 +0700

    demo change scene

---
+++ Demo3/Assets/script/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+//		Debug.Log ("start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		Debug.Log ("update");
+	}
+
+	public void loadLevel(string name){
+		Debug.Log ("loadLevel() "+name);
+		UnityEngine.SceneManagement.SceneManager.LoadScene (name);
+		//Application.LoadLevel (name);
+	}
+
+	public void quitGame(){
+		Application.Quit ();
+	}
+}
